87b6c01ae099079886c8066422f98f77c0ba7892
tankerjckk <tankerjckk@gmail.com>
2026-06-07 23:21:22 +0200

    Home-preload logo optimalization vol 1.2

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -828,15 +828,19 @@ function LoadingScreen() {
       {isLoading && (
         <motion.div
           className="fixed inset-0 z-[9999] flex items-center justify-center bg-black"
-          initial={{ opacity: 1 }}
-          exit={{
-            opacity: 0,
-            filter: "blur(12px)",
-            transition: {
-              duration: 0.8,
-              ease: "easeInOut",
-            },
-          }}
+initial={{
+  opacity: 0,
+  scale: 0.9,
+}}
+
+animate={{
+  opacity: 1,
+  scale: 1,
+}}
+
+exit={{
+  opacity: 0,
+}}
         >
           <motion.picture>
   <source
@@ -853,12 +857,10 @@ function LoadingScreen() {
             initial={{
               opacity: 0,
               scale: 0.9,
-              filter: "blur(10px)",
             }}
             animate={{
               opacity: 1,
               scale: 1,
-              filter: "blur(0px)",
             }}
             transition={{
               duration: 1,
